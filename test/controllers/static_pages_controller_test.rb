71453c55ac414170174843d85910ccf1e0a6453c
Joseph Lee <slee264@binghamton.edu>
2017-06-20 02:02:40 -0400

    static pages done

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+		assert_select "title", "Home | Ovan Capital"
+  end
+
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+		assert_select "title", "Help | Ovan Capital"
+  end
+
+	test "should get about" do
+		get static_pages_about_url
+		assert_response :success
+	end
+
+end
